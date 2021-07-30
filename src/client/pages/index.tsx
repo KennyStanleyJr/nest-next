1fbb590d4e8de270296bd0821916979c66e4c98d
Kenny Stanley <kpstanleyjr@gmail.com>
2021-07-30 12:04:51 -0500

    Added TailwindCSS

--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <>Hello, World!</>;
+  return (
+    <>
+      <div className="grid place-items-center h-screen bg-gray-900">
+        <h1 className="text-4xl text-white font-bold">Hello, World!</h1>
+      </div>
+    </>
+  );
 };
 
 export default Home;
---
+++ src/client/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './src/client/pages/**/*.{js,ts,jsx,tsx}',
+    './src/client/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
